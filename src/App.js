4c7f0edb2372b34ee995da8662874b55db369b97
tuit212 <asadbekanvarov9808@example.com>
2023-02-28 02:01:54 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import { Routes , Route } from "react-router-dom";
+
+import Left from "./components/Left/Left";
+
+
+function App() {
+  return (
+    <div className="App">
+    
+       {/* left */}
+       <Left/>
+
+       {/* center */}
+
+
+       {/* right */}
+    </div>
+  );
+}
+
+export default App;
